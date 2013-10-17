749343eda5cee3b5f1aaf9375b53e66cbd43ce35
Andrei <andrei@spreecommerce.com>
2013-10-17 14:44:29 -0400

    Added helpers file and parameters to send via API calls

--- lib/dotcom_order.rb
+++ lib/dotcom_order.rb
@@ -1,3 +1,5 @@
+require './lib/helpers'
+
 class DotcomOrder < DotcomConfig
   attr_reader :shipment
 
@@ -15,30 +17,106 @@ class DotcomOrder < DotcomConfig
     Nokogiri::XML::Builder.new do |xml|
       xml.orders {
         xml.order {
-          xml.__send__('order-number', number)
-          xml.__send__('ship_date', shipped_at)
-          xml.__send__('ship-method', shipping_method)
-          xml.__send__('invoice-number', order_number)
+          xml.send 'order-number',                 "#{order_number}-#{number}"
+          xml.send 'ship_date',                    shipped_at
+          xml.send 'ship-method',                  shipping_method
+          xml.send 'invoice-number',               0
+          xml.send 'order-date',                   Date.today.to_s
+          xml.send 'ship_via',                     ''
+          xml.send 'special-instructions',         ''
+          xml.send 'special-messaging',            ''
+          xml.send 'drop-ship',                    ''
+          xml.send 'ok-partial-ship',              ''
+          xml.send 'declared-value',               0
+          xml.send 'cancel-date',                  Date.today.to_s
+          xml.send 'total-tax',                    0
+          xml.send 'total-shipping-handling',      0
+          xml.send 'total-discount',               0
+          xml.send 'total-order-amount',           0
+          xml.send 'po-number',                    ''
+          xml.send 'salesman',                     ''
+          xml.send 'credit-card-number',           ''
+          xml.send 'credit-card-expiration',       ''
+          xml.send 'ad-code',                      ''
+          xml.send 'continuity-flag',              ''
+          xml.send 'freight-terms',                ''
+          xml.send 'department',                   ''
+          xml.send 'pay-terms',                    ''
+          xml.send 'tax-percent',                  0
+          xml.send 'asn-qualifier',                ''
+          xml.send 'gift-order-indicator',         ''
+          xml.send 'order-source',                 ''
+          xml.send 'promise-date',                 Date.today.to_s
+          xml.send 'third-party-account',          ''
+          xml.send 'priority',                     ''
+          xml.send 'retail-department',            ''
+          xml.send 'retail-store',                 ''
+          xml.send 'retail-vendor',                ''
+          xml.send 'pool',                         ''
 
-          xml.__send__('shipping-information') {
-            xml.name        shipping_full_name
-            xml.address1    shipping_address1
-            xml.address2    shipping_address2
-            xml.city        shipping_city
-            xml.state       shipping_state
-            xml.country     shipping_country
-            xml.zip         shipping_zipcode
-            xml.phone       shipping_phone
-            xml.email       email
+          xml.send('billing-information') {
+            xml.send 'billing-customer-number',    ''
+            xml.send 'billing-name',               'A'
+            xml.send 'billing-company',            ''
+            xml.send 'billing-address1',           'A'
+            xml.send 'billing-address2',           'A'
+            xml.send 'billing-address3',           'A'
+            xml.send 'billing-city',               'A'
+            xml.send 'billing-state',              ''
+            xml.send 'billing-zip',                20814
+            xml.send 'billing-country',            ''
+            xml.send 'billing-phone',              ''
+            xml.send 'billing-email',              ''
           }
 
-          xml.__send__('line-items') {
+          xml.send('custom-fields') {
+            xml.send 'custom-field-1',             ''
+            xml.send 'custom-field-2',             ''
+            xml.send 'custom-field-3',             ''
+            xml.send 'custom-field-4',             ''
+            xml.send 'custom-field-5',             ''
+          }
+
+          xml.send('shipping-information') {
+            xml.send 'shipping-customer-number',   ''
+            xml.send 'shipping-name',              shipping_full_name
+            xml.send 'shipping-address1',          shipping_address1
+            xml.send 'shipping-address2',          shipping_address2
+            xml.send 'shipping-address3',          ''
+            xml.send 'shipping-city',              shipping_city
+            xml.send 'shipping-state',             states_hash[shipping_state]
+            xml.send 'shipping-country',           shipping_country
+            xml.send 'shipping-iso-country-code',  ''
+            xml.send 'shipping-zip',               shipping_zipcode
+            xml.send 'shipping-phone',             shipping_phone
+            xml.send 'shipping-email',             email
+            xml.send 'shipping-company',           ''
+          }
+
+          xml.send('store-information') {
+            xml.send 'store-name',                 shipping_full_name
+            xml.send 'store-address1',             shipping_address1
+            xml.send 'store-address2',             shipping_address2
+            xml.send 'store-city',                 shipping_city
+            xml.send 'store-state',                'MD' # shipping_state convert to 2 chars
+            xml.send 'store-country',              shipping_country
+            xml.send 'store-zip',                  shipping_zipcode
+            xml.send 'store-phone',                shipping_phone
+          }          
+
+          xml.send('line-items') {
             shipment_items.each_with_index do |item, index|
-              xml.__send__('line_item') {
-                xml.__send__('client-item', item['name'])
-                xml.sku                     item['sku']
-                xml.quantity                item['quantity']
-                xml.price                   item['price']
+              xml.send('line-item') {
+                xml.send 'client-item',            item['variant_id']
+                xml.sku                            item['sku']
+                xml.quantity                       item['quantity']
+                xml.price                          item['price']
+                xml.tax                            0
+                xml.send 'shipping-handling',      0
+                xml.send 'line-number',            ''
+                xml.send 'gift-box-wrap-quantity', 0
+                xml.send 'gift-box-wrap-type',     ''
+
               }
             end
           }
@@ -59,11 +137,11 @@ class DotcomOrder < DotcomConfig
   end
 
   def shipped_at
-    shipment['shipped_at']
+    shipment['shipped_at'][0..-11]
   end
 
   def shipping_method
-    shipment['shipping_method']
+    shipment['shipping_method'][0..2]
   end
 
   def email
