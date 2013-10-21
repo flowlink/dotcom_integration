caedac842b1c7eae07ff7ee1e61f02cc0dccdee1
Andrei <andrei@spreecommerce.com>
2013-10-21 17:29:02 -0400

    More cleanup

--- lib/dotcom_shipment_tracking.rb
+++ lib/dotcom_shipment_tracking.rb
@@ -4,11 +4,5 @@ class DotcomShipmentTracking < DotcomConfig
   end
 
   def generate_xml
-    Nokogiri::XML::Builder.new do |xml|
-      xml.shipments {
-        xml.fromShipDate  '2010-10-01'
-        xml.toShipDate    '2013-10-01'
-      }
-    end.to_xml
   end
 end
